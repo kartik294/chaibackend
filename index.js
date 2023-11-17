bcc7a6d8797a022d5872062e4e494bcb213ca2d7
Kartik <kartikgeu@gmail.com>
2023-11-17 21:32:57 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/twitter", (req, res) => {
+  res.send("kartikkaushik.com");
+});
+
+app.get("/login", (req, res) => {
+  res.send("<h1>Please login at chai or code</h1>");
+});
+
+app.get("/youtube", (req, res) => {
+  res.send("<h2>Please watch Youtube</h2>");
+});
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${port}`);
+});
